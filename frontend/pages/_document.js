284468017ae8f51af5044fb23d14e98ce8704906
JoRuPeRu <johan.rutberg@iths.se>
2025-01-18 17:26:53 +0100

    Enhance favicon links and update disclaimer page headings; improve SEO metadata for category and topic pages

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -93,6 +93,10 @@ export default function Document() {
 
         {/* Favicon */}
         <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
+        <link rel="icon" type="image/png" href="/favicon.png" sizes="32x32" />
+        <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
 
         {/* Canonical URL */}
         <link rel="canonical" href="https://www.bamboosleeping.com" />
